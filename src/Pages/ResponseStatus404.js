cb1c5f2b819878888766fdcc82d9d01963203601
SergKnyazev <serg.knyazev77@gmail.com>
2021-01-08 11:43:07 +0300

    formatting ResponseStatus404.js

--- src/Pages/ResponseStatus404.js
+++ src/Pages/ResponseStatus404.js
@@ -1,16 +1,16 @@
 import React from 'react';
 
-const ResponseStatus404 = ({message404, statusMessage}) => (
-  <div style={
-    {
+const ResponseStatus404 = ({ message404, statusMessage }) => (
+  <div
+    style={{
       textAlign: 'center',
       marginTop: '2vmin',
       color: 'brown',
-    }
-  }>
-     <h2>404 --- ERROR --- 404</h2>
-     <p>{message404}</p>
-     <p>{statusMessage}</p>
+    }}
+  >
+    <h2>404 --- ERROR --- 404</h2>
+    <p>{message404}</p>
+    <p>{statusMessage}</p>
   </div>
 );
 
